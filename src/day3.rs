55ca5645d878f290846fe93ef71751c884deb3d9
Olivier Pinon <op@impero.com>
2021-12-04 01:29:21 +0100

    Better day3

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use std::simd::u16x16;
+use std::simd::{u16x16, Mask, SimdElement};
 
 #[aoc_generator(day3)]
 pub fn parse(input: &str) -> Result<Vec<u16>, std::num::ParseIntError> {
@@ -11,28 +11,47 @@ pub fn part1(input: &[u16]) -> u32 {
         32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1,
     ]);
     let zeros = u16x16::splat(0);
-    let ones = u16x16::splat(1);
-    let mut global_mask = 0; // dirty hack to cut irrelevant bits of epsilon
 
-    let mut total = u16x16::splat(0);
-    input.iter().for_each(|x| {
-        global_mask |= x;
-        let x = u16x16::splat(*x);
-        let and = pows & x;
-        let mask = and.lanes_gt(zeros);
-        let add = mask.select(ones, zeros);
-        total += add;
-    });
+    //// dirty hack to cut irrelevant bits
+    let global_mask: u16 = input
+        .iter()
+        .max()
+        .expect("Failed to find max")
+        .next_power_of_two()
+        - 1;
 
-    let other_count = u16x16::splat(input.len() as u16) - total;
-    let gamma_mask = total.lanes_gt(other_count);
-    let gamma: u16 = gamma_mask.select(pows, zeros).to_array().into_iter().sum();
-    let epsilon: u16 = !gamma & global_mask;
+    let g_mask = gamma_mask(&input);
+    let epsilon_mask = !g_mask;
+
+    let gamma: u16 = g_mask
+        .select(pows, zeros)
+        .to_array()
+        .into_iter()
+        .sum::<u16>()
+        & global_mask;
+    let epsilon: u16 = epsilon_mask
+        .select(pows, zeros)
+        .to_array()
+        .into_iter()
+        .sum::<u16>()
+        & global_mask;
 
     gamma as u32 * epsilon as u32
 }
 
-fn gamma_mask(input: &[u16]) -> [bool; 16] {
+#[aoc(day3, part2)]
+fn part2(input: &Vec<u16>) -> u32 {
+    let oxygen_rate = find_rate(input.clone(), BitCriteria::MostCommon) as u32;
+    let co2_rate = find_rate(input.clone(), BitCriteria::LeastCommon) as u32;
+    oxygen_rate * co2_rate
+}
+
+enum BitCriteria {
+    MostCommon,
+    LeastCommon,
+}
+
+fn gamma_mask(input: &[u16]) -> Mask<<i16 as SimdElement>::Mask, 16> {
     let pows = u16x16::from([
         32768, 16384, 8192, 4096, 2048, 1024, 512, 256, 128, 64, 32, 16, 8, 4, 2, 1,
     ]);
@@ -48,12 +67,7 @@ fn gamma_mask(input: &[u16]) -> [bool; 16] {
         total += add;
     });
     let other_count = u16x16::splat(input.len() as u16) - total;
-    total.lanes_ge(other_count).to_array()
-}
-
-enum BitCriteria {
-    MostCommon,
-    LeastCommon,
+    total.lanes_ge(other_count)
 }
 
 fn find_rate(mut input: Vec<u16>, criteria: BitCriteria) -> u16 {
@@ -72,7 +86,7 @@ fn find_rate(mut input: Vec<u16>, criteria: BitCriteria) -> u16 {
             return input[0];
         }
         bit -= 1;
-        let g_mask = gamma_mask(&input);
+        let g_mask = gamma_mask(&input).to_array();
         let target_bit = match criteria {
             BitCriteria::MostCommon => g_mask[15 - bit],
             BitCriteria::LeastCommon => !g_mask[15 - bit],
@@ -80,13 +94,6 @@ fn find_rate(mut input: Vec<u16>, criteria: BitCriteria) -> u16 {
         let mask = 2u16.pow(bit as u32);
         input.retain(|x| (x & mask > 0) == target_bit);
     }
-}
-
-#[aoc(day3, part2)]
-fn part2(input: &Vec<u16>) -> u32 {
-    let oxygen_rate = find_rate(input.clone(), BitCriteria::MostCommon) as u32;
-    let co2_rate = find_rate(input.clone(), BitCriteria::LeastCommon) as u32;
-    oxygen_rate * co2_rate
 }
 
 #[cfg(test)]
